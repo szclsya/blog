07818ac57ef7a9434862d615fceb553da85cd318
Leo Shen <i@szclsya.me>
2021-04-09 01:59:24 +0800

    posts: modify older posts
    - to fit the new look

--- generate-img.sh
+++ generate-img.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-for img in ./img/*/*; do
+SIZE="2500x2500"
+
+for img in ./img/*/*.jpg; do
     echo "Converting $img..."
     new_path=${img/'./img/'/'./static/img/'}
     mkdir -p "$(dirname $new_path)"
     # Generate minified JPG
-    convert $img -strip -interlace Plane -gaussian-blur 0.05 -quality 60% $new_path
+    convert $img -resize $SIZE -strip -interlace Plane -gaussian-blur 0.05 -quality 60% $new_path
     # Generate WebP
-    convert $img -quality 70 -strip -define webp:lossless=false -define webp:method=6 ${new_path/'.jpg'/'.webp'}
+    convert $img -resize $SIZE -quality 80 -strip -define webp:lossless=false -define webp:method=6 ${new_path/'.jpg'/'.webp'}
     # Generate AVIF
-    convert $img -quality 60 ${new_path/'.jpg'/'.avif'}
+    convert $img -resize $SIZE -quality 80 ${new_path/'.jpg'/'.avif'}
+done
+
+for img in ./img/*/*.png; do
+    echo "Converting $img..."
+    new_path=${img/'./img/'/'./static/img/'}
+    mkdir -p "$(dirname $new_path)"
+    # Copy original PNG
+    cp $img $new_path
+    # Generate AVIF
+    convert $img -resize $SIZE -strip ${new_path/'.png'/'.avif'}
 done
