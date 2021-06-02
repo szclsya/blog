0ea0fbfb6bdc5094e75d7bb5739c2ef280f495c9
Leo Shen <i@szclsya.me>
2021-06-02 17:45:30 +0800

    shortcode/img: add width and height info to imgs
    - tweak image generation script

--- generate-img.sh
+++ generate-img.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-SIZE="2500x2500"
+# \> in the end means it will only be resized if original image is bigger
+SIZE='2500x2500>'
 
 for img in $(find ./img | grep "jpg$"); do
     echo "Converting $img..."
-    new_path=${img/'./img/'/'./static/img/'}
+    new_path=${img/'./img/'/'./assets/img/'}
     mkdir -p "$(dirname $new_path)"
     # Generate minified JPG
-    convert $img -resize $SIZE -strip -interlace Plane -gaussian-blur 0.05 -quality 60% $new_path
+    convert $img -resize "$SIZE" -strip -interlace Plane -gaussian-blur 0.05 -quality 60% $new_path
     # Generate WebP
-    convert $img -resize $SIZE -quality 80 -strip -define webp:lossless=false -define webp:method=6 ${new_path/'.jpg'/'.webp'}
+    convert $img -resize "$SIZE" -strip -quality 80 -define webp:lossless=false -define webp:method=6 ${new_path/'.jpg'/'.webp'}
     # Generate AVIF
-    convert $img -resize $SIZE -quality 80 ${new_path/'.jpg'/'.avif'}
+    convert $img -resize $SIZE -strip -quality 80 ${new_path/'.jpg'/'.avif'}
 done
 
 for img in $(find ./img | grep "png$"); do
     echo "Converting $img..."
-    new_path=${img/'./img/'/'./static/img/'}
+    new_path=${img/'./img/'/'./assets/img/'}
     mkdir -p "$(dirname $new_path)"
     # Copy original PNG
-    convert $img -resize $SIZE -strip $new_path
+    convert $img -resize "$SIZE" -strip $new_path
+    # Use OptiPNG to minimize the result
+    optipng $new_path
+    # Generate lossless WebP
+    convert $img -resize "$SIZE" -strip -define webp:lossless=true ${new_path/'.png'/'.webp'}
     # Generate AVIF
-    convert $img -resize $SIZE -strip ${new_path/'.png'/'.avif'}
+    convert $img -resize "$SIZE" -strip ${new_path/'.png'/'.avif'}
 done
