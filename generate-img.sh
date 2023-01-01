1687f3d7908a6acc75190851c2ddc091adcb6f74
Leo Shen <i@szclsya.me>
2023-01-01 05:58:30 -0500

    content (2022-recap): update

--- generate-img.sh
+++ generate-img.sh
@@ -6,37 +6,55 @@ SIZE='2500x2500>'
 for img in $(find ./img | grep "avif$"); do
     echo "Converting $img..."
     new_path=${img/'./img/'/'./assets/img/'}
-    mkdir -p "$(dirname $new_path)"
+    mkdir -p "$(dirname "$new_path")"
     # Generate minified JPG
-    convert $img -resize "$SIZE" -strip -interlace Plane -gaussian-blur 0.05 -quality 60% ${new_path/'.avif'/'.jpg'}
+    if [[ ! -e ${new_path/'.avif'/'.jpg'} ]]; then
+        convert "$img" -resize "$SIZE" -strip -interlace Plane -gaussian-blur 0.05 -quality 60% "${new_path/'.avif'/'.jpg'}"
+    fi
     # Generate WebP
-    convert $img -resize "$SIZE" -strip -quality 80 -define webp:lossless=false -define webp:method=6 ${new_path/'.avif'/'.webp'}
-    # Generate AVIF
-    convert $img -resize $SIZE -strip -quality 80 $new_path
+    if [[ ! -e ${new_path/'.avif'/'.webp'} ]]; then
+        convert "$img" -resize "$SIZE" -strip -quality 80 -define webp:lossless=false -define webp:method=6 "${new_path/'.avif'/'.webp'}"
+    fi
+    # Generate minified AVIF
+    if [[ ! -e $new_path ]]; then
+        convert "$img" -resize $SIZE -strip -quality 80 "$new_path"
+    fi
 done
 
 for img in $(find ./img | grep "jpg$"); do
     echo "Converting $img..."
     new_path=${img/'./img/'/'./assets/img/'}
-    mkdir -p "$(dirname $new_path)"
+    mkdir -p "$(dirname "$new_path")"
     # Generate minified JPG
-    convert $img -resize "$SIZE" -strip -interlace Plane -gaussian-blur 0.05 -quality 60% $new_path
+    if [[ ! -e $new_path ]]; then
+        convert "$img" -resize "$SIZE" -strip -interlace Plane -gaussian-blur 0.05 -quality 60% "$new_path"
+    fi
     # Generate WebP
-    convert $img -resize "$SIZE" -strip -quality 80 -define webp:lossless=false -define webp:method=6 ${new_path/'.jpg'/'.webp'}
+    if [[ ! -e ${new_path/'.jpg'/'.webp'} ]]; then
+        convert "$img" -resize "$SIZE" -strip -quality 80 -define webp:lossless=false -define webp:method=6 "${new_path/'.jpg'/'.webp'}"
+    fi
     # Generate AVIF
-    convert $img -resize $SIZE -strip -quality 80 ${new_path/'.jpg'/'.avif'}
+    if [[ ! -e ${new_path/'.jpg'/'.avif'} ]]; then
+        convert "$img" -resize $SIZE -strip -quality 80 "${new_path/'.jpg'/'.avif'}"
+    fi
 done
 
 for img in $(find ./img | grep "png$"); do
     echo "Converting $img..."
     new_path=${img/'./img/'/'./assets/img/'}
-    mkdir -p "$(dirname $new_path)"
+    mkdir -p "$(dirname "$new_path")"
     # Copy original PNG
-    convert $img -resize "$SIZE" -strip $new_path
-    # Use OptiPNG to minimize the result
-    optipng $new_path
+    if [[ ! -e $new_path ]]; then
+        convert "$img" -resize "$SIZE" -strip "$new_path"
+        # Use OptiPNG to minimize the result
+        optipng "$new_path"
+    fi
     # Generate lossless WebP
-    convert $img -resize "$SIZE" -strip -define webp:lossless=true ${new_path/'.png'/'.webp'}
+    if [[ ! -e ${new_path/'.png'/'.webp'} ]]; then
+        convert "$img" -resize "$SIZE" -strip -define webp:lossless=true "${new_path/'.png'/'.webp'}"
+    fi
     # Generate AVIF
-    convert $img -resize "$SIZE" -strip ${new_path/'.png'/'.avif'}
+    if [[ ! -e ${new_path/'.png'/'.avif'} ]]; then
+        convert "$img" -resize "$SIZE" -strip "${new_path/'.png'/'.avif'}"
+    fi
 done
